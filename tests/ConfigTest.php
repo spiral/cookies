3d4c6a8fbed92cf5bc4d6bf9db3047338d75a84a
spiralbot <ci-bot@spiralscout.com>
2025-01-22 15:13:31 +0000

    prepare release

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -19,8 +19,8 @@ class ConfigTest extends TestCase
 
         ]);
 
-        self::assertSame(CookiesConfig::COOKIE_ENCRYPT, $c->getProtectionMethod());
-        self::assertSame(['PHPSESSID', 'csrf-token'], $c->getExcludedCookies());
+        $this->assertSame(CookiesConfig::COOKIE_ENCRYPT, $c->getProtectionMethod());
+        $this->assertSame(['PHPSESSID', 'csrf-token'], $c->getExcludedCookies());
     }
 
     public function testCookieDomain(): void
@@ -29,18 +29,18 @@ class ConfigTest extends TestCase
             'domain' => '.%s',
         ]);
 
-        self::assertSame('.domain.com', $c->resolveDomain(new Uri('http://domain.com/')));
-        self::assertSame('.domain.com', $c->resolveDomain(new Uri('https://domain.com/')));
-        self::assertSame('.domain.com', $c->resolveDomain(new Uri('https://domain.com:9090/')));
-        self::assertNull($c->resolveDomain(new Uri('/')));
-        self::assertSame('localhost', $c->resolveDomain(new Uri('localhost:9090/')));
+        $this->assertSame('.domain.com', $c->resolveDomain(new Uri('http://domain.com/')));
+        $this->assertSame('.domain.com', $c->resolveDomain(new Uri('https://domain.com/')));
+        $this->assertSame('.domain.com', $c->resolveDomain(new Uri('https://domain.com:9090/')));
+        $this->assertSame(null, $c->resolveDomain(new Uri('/')));
+        $this->assertSame('localhost', $c->resolveDomain(new Uri('localhost:9090/')));
 
-        self::assertSame('192.169.1.10', $c->resolveDomain(new Uri('http://192.169.1.10:8080/')));
+        $this->assertSame('192.169.1.10', $c->resolveDomain(new Uri('http://192.169.1.10:8080/')));
 
         $c = new CookiesConfig([
             'domain' => '.doo.com',
         ]);
 
-        self::assertSame('.doo.com', $c->resolveDomain(new Uri('http://domain.com/')));
+        $this->assertSame('.doo.com', $c->resolveDomain(new Uri('http://domain.com/')));
     }
 }
